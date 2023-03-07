f4710cc8860bdbf2b8edc042c6c1a4322c533723
Donovan Gomez <donovanjuniogomez@gmail.com>
2023-03-06 20:51:24 -0800

    refactor featuredseiyuu

--- src/components/FeaturedSeiyuu.tsx
+++ src/components/FeaturedSeiyuu.tsx
@@ -1,36 +1,36 @@
-import SeiyuuCard from "./SeiyuuCard"
+import SeiyuuCard from "./SeiyuuCard";
 
 interface seiyuuProps {
-    seiyuu: {
-      mal_id: number,
-      name: string,
-      website_url: string,
-      url: string,
-      images: {
-        jpg: {
-          image_url: string
-        }
-      }
-    }[]
-  }
-  
-
-const FeaturedSeiyuu: React.FC<seiyuuProps> = ({seiyuu}) => {
-    return(
-        <>
-          <h2>This is the seiyuu container</h2>
-          {seiyuu.map((seiyuu) => (
-            <div key={seiyuu.mal_id}>
-              <SeiyuuCard
-                name={seiyuu.name}
-                website_url={seiyuu.website_url}
-                url={seiyuu.url}
-                image_url={seiyuu.images.jpg.image_url}
-              />
-            </div>
-          ))}
-        </>
-    )
+  seiyuu: {
+    mal_id: number;
+    name: string;
+    website_url: string;
+    url: string;
+    images: {
+      jpg: {
+        image_url: string;
+      };
+    };
+  }[];
 }
+
+const FeaturedSeiyuu: React.FC<seiyuuProps> = ({ seiyuu }) => {
+  return (
+    <section>
+      <div className="flex flex-wrap gap-2 justify-center">
+        {seiyuu.map((seiyuu) => (
+          <div key={seiyuu.mal_id}>
+            <SeiyuuCard
+              name={seiyuu.name}
+              website_url={seiyuu.website_url}
+              url={seiyuu.url}
+              image_url={seiyuu.images.jpg.image_url}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default FeaturedSeiyuu;
