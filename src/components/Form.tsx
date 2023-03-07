e76a6b799c80f63d633a4da8128867937b30df31
Donovan Gomez <donovanjuniogomez@gmail.com>
2023-03-07 00:33:57 -0800

    add search functionality

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,44 @@
 import React from "react";
 
 const Form: React.FC = () => {
-  const [inputText, setInputText] = React.useState("Uwu");
+  const [searchInput, setSearchInput] = React.useState("Uwu");
+  const [seiyuu, setSeiyuu] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
 
-  const inputTextHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(e.target.value);
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    searchSeiyuu(searchInput);
+    setSearchInput("");
+  };
+
+  const searchSeiyuu = async (query: string) => {
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `https://api.jikan.moe/v4/people?q=${query}&order_by=favorites&sort=desc`
+      );
+      const data = await res.json();
+      setSeiyuu(data.data);
+      console.log(seiyuu)
+    } catch (e) {
+      console.log("error!!!", e);
+      setError(error);
+    }
+    console.log(seiyuu);
+    setLoading(false);
   };
 
   return (
     <form>
-      <input type="text" placeholder="try Rikako Aida" onChange={inputTextHandler} value={inputText} />
-      <button>Search</button>
-      <div>
-        {inputText}
-      </div>
+      <input
+        type="text"
+        placeholder="try Rikako Aida"
+        onChange={(e) => setSearchInput(e.target.value)}
+        value={searchInput}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div></div>
     </form>
   );
 };
